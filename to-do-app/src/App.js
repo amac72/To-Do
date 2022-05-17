7648b9d9db992619695576011c833753d0adabdb
Austin M <93621884+amac72@users.noreply.github.com>
2022-05-16 20:52:44 -0500

    Create list directory

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Header from './header/Header.js';
-import Main from './Main.js';
+import List from './list/List.js';
 
 function App() {
   document.title = "To-Do"
   return (
     <div className="App">
       <Header />
-      <Main />
+      <List />
     </div>
   );
 }
