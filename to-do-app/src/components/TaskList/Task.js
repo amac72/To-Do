a93d12617b52f1e263b99ab0fb6bcb337da5e3d2
Austin M <93621884+austinmclain@users.noreply.github.com>
2022-08-11 17:20:44 -0500

    Configure git to recognize capitalization

---
+++ to-do-app/src/components/TaskList/Task.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import './Task.css';
+
+function Task(props) {
+    function generateCheckbox(completed) {
+        if (completed) {
+            return (
+                <Button className="completeButton" variant="secondary" onClick={() => props.updateTask(false, 'completed', props.task.id)}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-check2-square" viewBox="0 0 16 16">
+                    <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z" />
+                    <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
+                </svg></Button>
+            )
+        } else {
+            return (
+                <Button className="completeButton" variant="secondary" onClick={() => props.updateTask(true, 'completed', props.task.id)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                </svg></Button>
+            )
+        }
+    }
+
+    return (
+        <div className="TaskFrame container-lg">
+            <input className="description" onChange={(event) => props.updateTask(event.target.value, 'description', props.task.id)} value={props.task.description} placeholder="Enter task here" />
+            <input type="date" className="date" onChange={(event) => props.updateTask(event.target.value, 'date', props.task.id)} value={props.task.date} />
+            <Button className="deleteButton gray-button" variant="secondary" onClick={() => props.deleteTask()}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+            </svg></Button>
+            {generateCheckbox(props.task.completed)}
+        </div>
+    )
+}
+
+export default Task;
